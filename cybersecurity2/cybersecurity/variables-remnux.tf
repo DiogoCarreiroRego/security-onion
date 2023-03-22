dc65edd80ba93077f3ff0db2ffd62724170a4468
Diogo Rego <diego.arruda.amaral+github@gmail.com>
2023-03-22 21:11:14 -0100

    22.03.2023

---
+++ cybersecurity2/cybersecurity/variables-remnux.tf
@@ -0,0 +1,28 @@
+variable "remnux_ami" {
+  type = string
+  default = "ami-00118dd8add6c7104"
+}
+
+variable "remnux_type" {
+  type = string
+  default = "m5.large"
+}
+
+variable "remnux_userpw" {
+  description = "Insert the pasword for the user remnux"
+  type = string
+  sensitive   = true
+  default = "Passw0rd"
+}
+
+variable "cloud-config-remnux" {
+  default = "cloud-config-remnux.sh"
+}
+
+variable "config-remnux" {
+  default = "config-remnux.sh"
+}
+
+variable "remnux-change-password" {
+  default = "remnux-change-password.sh"
+}
---
+++ cybersecurity2/cybersecurity/variables-sift.tf
@@ -0,0 +1,28 @@
+variable "sift_ami" {
+  type = string
+  default = "ami-0428a08aa86743ee8"
+}
+
+variable "sift_type" {
+  type = string
+  default = "m5.large"
+}
+
+variable "sift_userpw" {
+  description = "Insert the pasword for the user sift"
+  type = string
+  sensitive   = true
+  default = "Passw0rd"
+}
+
+variable "cloud-config-sift" {
+  default = "cloud-config-sift.sh"
+}
+
+variable "config-sift" {
+  default = "config-sift.sh"
+}
+
+variable "sift-change-password" {
+  default = "sift-change-password.sh"
+}
